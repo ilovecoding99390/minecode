58241c987cc5cbdc5fd6ab1199e799bf8da2f35d
Zekecodes <roseybrauch@hotmail.com>
2025-05-19 21:43:35 -0400

    Update setup.py
    i added the full setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,20 @@ from setuptools import setup, find_packages
 setup(  
     name="minecode",  
     version="0.1.0",  
-    author="Your Name",  
+    author="Ezekiel Nogle",  
     description="A Python module that uses bed_apy",  
     long_description=open("README.md").read(),  
     long_description_content_type="text/markdown",  
     packages=find_packages(),  
-    install_requires=[],  # Add dependencies here  
+    install_requires=[
+        bed_apy,
+        asyncio,
+        math,
+        json,
+        PIL,
+        random
+        
+    ],  # Add dependencies here  
     classifiers=[  
         "Programming Language :: Python :: 3",  
         "License :: OSI Approved :: GNU General Public License v3 (GPL-3.0)",  

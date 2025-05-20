a0def6f28ba64c8e82db2bca9e9e53870f223107
Zekecodes <roseybrauch@hotmail.com>
2025-05-20 18:30:50 -0400

    fixed so pypi will work better

--- setup.py
+++ setup.py
@@ -6,20 +6,17 @@ setup(
     description="A Python module that uses bed_apy",  
     long_description=open("README.md").read(),  
     long_description_content_type="text/markdown",  
-    packages=find_packages(),  
-    install_requires=[
-        bed_apy,
-        asyncio,
-        math,
-        json,
-        pillow,
-        random
-        
-    ],  # Add dependencies here  
+    packages=find_packages(where="src"),  # Finds packages under src/
+    package_dir={"": "src"},              # Tells distutils that packages are under src/  
+    install_requires = [
+        "numpy",
+        "pillow",
+        "requests"
+    ],
     classifiers=[  
         "Programming Language :: Python :: 3",  
         "License :: OSI Approved :: GNU General Public License v3 (GPL-3.0)",  
         "Operating System :: OS Independent",  
     ],  
-    python_requires=">=3.8",  
+    python_requires=">=3.8"
 )  

060ce016a1549917a639893bd351c2da145c91cc
Zekecodes <roseybrauch@hotmail.com>
2025-05-19 21:38:41 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(  
+    name="minecode",  
+    version="0.1.0",  
+    author="Your Name",  
+    description="A Python module that uses bed_apy",  
+    long_description=open("README.md").read(),  
+    long_description_content_type="text/markdown",  
+    packages=find_packages(),  
+    install_requires=[],  # Add dependencies here  
+    classifiers=[  
+        "Programming Language :: Python :: 3",  
+        "License :: OSI Approved :: GNU General Public License v3 (GPL-3.0)",  
+        "Operating System :: OS Independent",  
+    ],  
+    python_requires=">=3.8",  
+)  
